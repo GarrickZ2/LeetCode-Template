4a382aae8903742834a7d1824d78378f9d4dc9e1
Zixuan(Garrick) Zhang <garrickz2018@gmail.com>
2022-06-25 15:03:34 -0400

    c1

--- src/templates/UniFind.java
+++ src/templates/UniFind.java
@@ -1,4 +1,47 @@
 package templates;
 
+import java.util.Arrays;
+
 public class UniFind {
+	int[] parent;
+	int[] rank;
+	int size;
+
+	public UniFind(int n) {
+		this.parent = new int[n];
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+		this.rank = new int[n];
+		Arrays.fill(rank, 1);
+		this.size = n;
+	}
+
+	public int getRoot(int x) {
+		int root = parent[x];
+		while (root != x) {
+			x = root;
+			root = parent[x];
+		}
+		return root;
+	}
+
+	public boolean union(int x, int y) {
+		int rootX = getRoot(x);
+		int rootY = getRoot(y);
+		if (rootX == rootY) return false;
+		if (rank[rootX] >= rank[rootY]) {
+			parent[rootY] = rootX;
+			rank[rootX] += rank[rootY];
+		} else {
+			parent[rootX] = rootY;
+			rank[rootY] += rank[rootX];
+		}
+		size --;
+		return true;
+	}
+
+	public int getSize() {
+		return size;
+	}
 }
